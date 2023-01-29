06206eb389e06281dedf0cd6fe234e62214b1a29
Piyush Katyal <95082977+Piyush-Katyal@users.noreply.github.com>
2023-01-29 15:33:03 +0530

    Add files via upload
    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/"+ process.env.MAILCHIMP_LIST_ID;
+
+    var options = {
+        method: "POST",
+        auth: process.env.MAILCHIMP_API_KEY_AUTH
+    }
+
+    const rqst = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            var statusCode = response.statusCode;
+            if (200<=statusCode & statusCode<=299){
+                res.sendFile(__dirname + "/success.html");
+            }
+            else{
+                res.sendFile(__dirname + "/failure.html");
+            }
+        });
+    });
+
+    rqst.write(jsonData);
+    rqst.end();
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is online");
+});
